285117c1b4b9aea3359976b262fca7d4dd2db382
satyamagrawal17 <satyam.agrawal_ftc@external.swiggy.in>
2025-03-10 11:18:02 +0530

    Created method for initialize Dynamodb and create its tables

--- go.mod
+++ go.mod
@@ -9,6 +9,21 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.9 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.62 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.41.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.29.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.17 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- internal/database/dynamodb.go
+++ internal/database/dynamodb.go
@@ -1,1 +1,167 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	configure "ordering_service/internal/config"
+)
+
+type DynamoDB struct {
+	DB                *dynamodb.Client
+	OrderTableName    string
+	MenuItemTableName string
+}
+
+func InitDynamoDB() (*DynamoDB, error) {
+	cfg, err := configure.LoadConfig()
+
+	// Create custom resolver for local DynamoDB
+	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			PartitionID:   "aws",
+			URL:           cfg.DynamoEndpoint,
+			SigningRegion: cfg.DynamoRegion,
+		}, nil
+	})
+
+	// Load AWS configuration with local settings
+	awsConfig, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(cfg.DynamoRegion),
+		config.WithEndpointResolverWithOptions(customResolver),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			cfg.DynamoAccessKey,
+			cfg.DynamoSecretKey,
+			"local-session",
+		)),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load local DynamoDB config: %w", err)
+	}
+	// Create DynamoDB client
+	newDbInstance := &DynamoDB{
+		DB:                dynamodb.NewFromConfig(awsConfig),
+		OrderTableName:    "orders",
+		MenuItemTableName: "menu_items",
+	}
+	if err := newDbInstance.createOrderTable(); err != nil {
+		return nil, fmt.Errorf("failed to create table: %w", err)
+	}
+
+	if err := newDbInstance.CreateMenuItemTable(); err != nil {
+		return nil, fmt.Errorf("failed to create table: %w", err)
+	}
+	return newDbInstance, nil
+}
+
+func (d *DynamoDB) createOrderTable() error {
+	// Check if the table already exists
+	exists, err := d.doesTableExists(d.OrderTableName)
+	if err != nil {
+		return fmt.Errorf("failed to check if order table exists: %w", err)
+	}
+
+	if exists {
+		// Table already exists, no need to create
+		return nil
+	}
+
+	// Define the table schema
+	input := &dynamodb.CreateTableInput{
+		TableName: &d.OrderTableName,
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("Id"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("Id"),
+				KeyType:       types.KeyTypeHash,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+
+	// Create the table
+	_, err = d.DB.CreateTable(context.TODO(), input)
+	if err != nil {
+		return fmt.Errorf("failed to create table: %w", err)
+	}
+
+	return nil
+}
+
+func (d *DynamoDB) CreateMenuItemTable() error {
+	// Check if the table already exists
+	exists, err := d.doesTableExists(d.MenuItemTableName)
+	if err != nil {
+		return fmt.Errorf("failed to check if menu item table exists: %w", err)
+	}
+
+	if exists {
+		// Table already exists, no need to create
+		return nil
+	}
+
+	// Define the table schema
+	input := &dynamodb.CreateTableInput{
+		TableName: &d.MenuItemTableName,
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("Id"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("Id"),
+				KeyType:       types.KeyTypeHash,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+
+	// Create the table
+	_, err = d.DB.CreateTable(context.TODO(), input)
+	if err != nil {
+		return fmt.Errorf("failed to create table: %w", err)
+	}
+
+	return nil
+}
+
+func (d *DynamoDB) doesTableExists(tableName string) (bool, error) {
+
+	input := &dynamodb.ListTablesInput{}
+	for {
+		result, err := d.DB.ListTables(context.TODO(), input)
+		if err != nil {
+			return false, fmt.Errorf("failed to list tables: %w", err)
+		}
+
+		for _, name := range result.TableNames {
+			if name == tableName {
+				return true, nil
+			}
+		}
+
+		if result.LastEvaluatedTableName == nil {
+			break
+		}
+		input.ExclusiveStartTableName = result.LastEvaluatedTableName
+	}
+
+	return false, nil
+}
